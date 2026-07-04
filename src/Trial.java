281fb0a30c6167d103a04ded98e1e871f579aa3e
arun singh <aruns5061@gmail.com>
2026-07-04 15:04:16 +0530

    save var and datatypes

--- src/Trial.java
+++ src/Trial.java
@@ -1,7 +1,6 @@
-package PACKAGE_NAME;
 
 public class Trial {
     public static void main(String[] args){
-        System.out.println("arun is back in java ");
+        System.out.println("arun is back i n java ");
     }
 }
---
+++ src/variablesAndDataTypes.java
@@ -0,0 +1,23 @@
+public class variablesAndDataTypes {
+    public static void main(String[] args) {
+        // Variable declaration and initialization
+        int age = 25; // integer variable
+        double height = 5.9; // double variable
+        char grade = 'A'; // character variable
+        boolean isStudent = true; // boolean variable
+        String name = "Arun"; // String variable
+
+        // Output the values of the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+        /*int a,b,c;
+        a=10;
+        b=20;
+        c=40; approach to declare and initialize multiple variables of the same type*/
+
+    }
+    
+}
