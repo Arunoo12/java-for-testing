4b55e39b63b1099a53d9a559b5fab33dc9222206
arun singh <aruns5061@gmail.com>
2026-08-03 16:10:14 +0530

    save approach 2 to revString

---
+++ src/revStringA2.java
@@ -0,0 +1,14 @@
+public class revStringA2 {
+    public static void main(String[] args) {
+        String word = "java";
+        String rev = "";
+        // now convert into a char array
+        char a[] = word.toCharArray();
+        // System.out.println(a.indexOf('j'));
+        for (int i = a.length - 1; i >= 0; i--) {
+            rev = rev + a[i];
+
+        }
+        System.out.println(rev);
+    }
+}
